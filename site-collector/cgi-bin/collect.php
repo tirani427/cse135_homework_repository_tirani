7a236da15d3eabff6597050a5b70d50902081000
tiairani <tia@ivector.co>
2026-03-08 21:10:29 -0700

    adjustment for collect.php

--- site-collector/cgi-bin/collect.php
+++ site-collector/cgi-bin/collect.php
@@ -1,8 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: text/plain; charset=utf-8");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Max-Age: 86400");
 
 if($_SERVER["REQUEST_METHOD"] === "OPTIONS"){
     http_response_code(204);
